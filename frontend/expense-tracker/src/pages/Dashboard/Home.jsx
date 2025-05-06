4929b01db8fc60083f9d78fbd2d6e1045032bbcf
prabhjot-2 <prabh1146@gmail.com>
2025-05-06 23:36:20 +0530

    Add RecentTransactions component and integrate it into Home: create RecentTransactions for displaying recent transaction data; implement TranactionInfoCard for individual transaction details; enhance Home component to utilize RecentTransactions.

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -8,6 +8,7 @@ import InfoCard from '../../components/Cards/InfoCard';
 import {LuHandCoins, LuWalletMinimal} from "react-icons/lu"
 import { IoMdCard } from 'react-icons/io';
 import { addThousandsSeparator } from '../../utils/helper';
+import RecentTransactions from '../../components/Dashboard/RecentTransactions';
 
 const Home = () => {
   useUserAuth();
@@ -46,7 +47,10 @@ const Home = () => {
         <InfoCard icon={<IoMdCard/>} label="Total Balance" value={addThousandsSeparator(dashboardData?.totalBalance || 0)} color="bg-primary"/>
         <InfoCard icon={<LuWalletMinimal/>} label="Total Income" value={addThousandsSeparator(dashboardData?.totalIncome || 0)} color="bg-orange-500"/>
         <InfoCard icon={<LuHandCoins/>} label="Total Expense" value={addThousandsSeparator(dashboardData?.totalExpenses || 0)} color="bg-red-500"/>
+      </div>
 
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-6'>
+        <RecentTransactions transactions={dashboardData?.recentTransactions} onSeeMore={()=>navigate("/expense")}/>
       </div>
       </div>
     </DashboardLayout>
