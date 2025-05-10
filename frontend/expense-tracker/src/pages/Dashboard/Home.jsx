3debdcf4ebb6017e75ce6ef2c184a9162d373773
prabhjot-2 <prabh1146@gmail.com>
2025-05-10 22:52:35 +0530

    Add RecentIncomeWithChart component and integrate it into Home: create RecentIncomeWithChart for displaying income data over the last 60 days; update Home component to utilize RecentIncomeWithChart with relevant data.

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -12,6 +12,7 @@ import RecentTransactions from '../../components/Dashboard/RecentTransactions';
 import FinanceOverview from '../../components/Dashboard/FinanceOverview';
 import ExpenseTransactions from '../../components/Dashboard/ExpenseTransactions';
 import Last30DaysExpenses from '../../components/Dashboard/Last30DaysExpenses';
+import RecentIncomeWithChart from '../../components/Dashboard/RecentIncomeWithChart';
 
 const Home = () => {
   useUserAuth();
@@ -63,6 +64,11 @@ const Home = () => {
 
         <Last30DaysExpenses
           data={dashboardData?.last30DaysExpenses?.transactions || []}
+          />
+
+        <RecentIncomeWithChart
+          data={dashboardData?.last60DaysIncome?.transactions?.slice(0,4) || []}
+          totalIncome={dashboardData?.totalIncome || 0}
           />
 
       </div>
