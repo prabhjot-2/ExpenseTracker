3e4a704a3d4885f4a023cacd920d3dfc634d57e4
prabhjot-2 <prabh1146@gmail.com>
2025-05-13 22:24:51 +0530

    Refactor EmojiPickerPopup styles: enhance layout and styling for icon display and close button for improved user experience.

--- frontend/expense-tracker/src/components/EmojiPickerPopup.jsx
+++ frontend/expense-tracker/src/components/EmojiPickerPopup.jsx
@@ -10,8 +10,8 @@ const EmojiPickerPopup = ({icon,onSelect}) => {
     <div className="flex items-center gap-4 cursor-pointer"
         onClick={()=>setIsOpen(true)}
     >
-        <div className="">
-        {icon ? (<img src={icon} alt="Icon" className=""/>
+        <div className="w-12 h-12 flex items-center justify-center text-2xl bg-purple-50 text-primary rounded-lg">
+        {icon ? (<img src={icon} alt="Icon" className="w-12 h-12"/>
 
         ):(
             <LuImage/>
@@ -25,7 +25,7 @@ const EmojiPickerPopup = ({icon,onSelect}) => {
     {isOpen && (
         <div className=""
             >
-            <button  className=" "
+            <button  className="w-7 h-7 flex items-center justify-center bg-white border border-gray-200 rounded-full absolute -top-2 -right-2 z-10 cursor-pointer "
             onClick={()=>setIsOpen(false)}
             >
                 <LuX/>
