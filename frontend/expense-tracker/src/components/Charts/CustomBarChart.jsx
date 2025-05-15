48972d296c059ba89e860470c8f1c00c986fd2be
prabhjot-2 <prabh1146@gmail.com>
2025-05-15 23:14:20 +0530

    Update currency symbol to Indian Rupee (₹) in InfoCard, TranactionInfoCard, CustomBarChart, CustomLineChart, and CustomToolTip components for consistent currency representation.

--- frontend/expense-tracker/src/components/Charts/CustomBarChart.jsx
+++ frontend/expense-tracker/src/components/Charts/CustomBarChart.jsx
@@ -16,7 +16,7 @@ const CustomBarChart = ({data}) => {
             {payload[0].payload.category}
           </p>
           <p className='text-sm text-gray-600'>
-            Amount: <span className='text-sm font-medium text-gray-900'> ${payload[0].payload.amount}</span>
+            Amount: <span className='text-sm font-medium text-gray-900'> ₹{payload[0].payload.amount}</span>
           </p>
         </div>
       );
